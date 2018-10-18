50c50a0d5ae082421807aeeb6965cc77f05ca35f
watch-repairman <36956119+watch-repairman@users.noreply.github.com>
2018-10-18 10:49:14 +0800

    Update and rename Matlab exercise to Exercise3_4.m

---
+++ Exercise3_4.m
@@ -0,0 +1,25 @@
+//Exercise 3.4
+function y=bijiao(max,min)      %输入两个值
+if max<min     
+    max=min;
+elseif max==min;
+    disp('They are equal!');
+else 
+end      
+max
+
+
+
+%命令行窗口验证代码：
+>> bijiao(3,7)
+
+max =
+
+     7
+
+>> bijiao(5,5)
+They are equal!
+
+max =
+
+     5
